b08a2ead891dd6efadf41ba44bff42e16c026b2b
neodevelop <juan@makingdevs.com>
2016-08-25 17:24:54 -0500

    Renaming class

--- src/test/groovy/com/makingdevs/TodoFeature.groovy
+++ src/test/groovy/com/makingdevs/TodoFeature.groovy
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = [ "classpath:features/todo.feature" ], strict = true, plugin = [ "html:build/test-report" ])
-class HomeFeature {
+class TodoFeature {
 
 }
 
