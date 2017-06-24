a1b411a2a7376111705eb66b76af050930dc02f3
neodevelop <juan@makingdevs.com>
2017-06-23 20:11:59 -0500

    Updating driverm adjusting selectors

--- geb_scripts/geb_script_simple.groovy
+++ geb_scripts/geb_script_simple.groovy
@@ -1,21 +1,20 @@
 @Grapes([
 @Grab("org.gebish:geb-core:0.13.1"),
 //@Grab("org.seleniumhq.selenium:selenium-firefox-driver:2.53.1"),
-@Grab("org.seleniumhq.selenium:selenium-chrome-driver:2.53.1"),
-@Grab("org.seleniumhq.selenium:selenium-support:2.53.1")
+@Grab("org.seleniumhq.selenium:selenium-chrome-driver:3.4.0"),
+@Grab("org.seleniumhq.selenium:selenium-support:3.4.0")
 ])
 
 import geb.Browser
 
-System.setProperty("webdriver.chrome.driver","/Users/neodevelop/Downloads/chromedriver")
+System.setProperty("webdriver.chrome.driver","/Users/neodevelop/Desktop/workshop-functional-testing/build/webdriver/chromedriver/chromedriver")
 
 Browser.drive {
   go "http://gebish.org"
 
   assert title == "Geb - Very Groovy Browser Automation"
 
-  $("#sidebar .sidemenu a", text: "jQuery-like API").click()
-
-  assert $("#main h1")*.text() == ["Navigating Content", "Form Control Shortcuts"]
-  assert $("#sidebar .sidemenu a", text: "jQuery-like API").parent().hasClass("selected")
+  $("body > div.ui.inverted.vertical.masthead.center.aligned.segment > div.ui.large.inverted.secondary.horizontal.pointing.menu > div > a.manuals.item", text: "Manual").click()
+  $("#manuals-menu > div > a:nth-child(1)").first().click()
+  assert $("div#header h1").text() == "The Book Of Geb"
 }
