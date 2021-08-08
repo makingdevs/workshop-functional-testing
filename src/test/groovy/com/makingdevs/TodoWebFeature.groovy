c8daf4435859e6c826f1046839841999cf9d2b14
Jose Juan Reyes <neodevelop@gmail.com>
2021-08-08 15:19:20 -0500

    The packages from Cucumber has changed

--- src/test/groovy/com/makingdevs/TodoWebFeature.groovy
+++ src/test/groovy/com/makingdevs/TodoWebFeature.groovy
@@ -2,8 +2,8 @@ package com.makingdevs
 
 import org.junit.runner.RunWith
 
-import cucumber.api.CucumberOptions
-import cucumber.api.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import io.cucumber.junit.Cucumber
 
 @RunWith(Cucumber)
 @CucumberOptions(features = [ "classpath:cucumber/resources/features/todo_web.feature" ], strict = true, plugin = [ "html:build/test-report" ])
