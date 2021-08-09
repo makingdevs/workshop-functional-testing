49e07581e0b89b86f6a1a311517b646a533f6ab4
Jose Juan Reyes <neodevelop@gmail.com>
2021-08-08 22:56:06 -0500

    Add a simple kotlin test

---
+++ src/test/kotlin/com/makingdevs/FizzBuzzTest.kt
@@ -0,0 +1,14 @@
+package com.makingdevs
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class FizzBuzzTest : FunSpec() {
+
+  init {
+    test("my first test") {
+      6 + 2 shouldBe 4
+    }
+  }
+
+}
