b3edab8ec1e98f1a2ac35ede95b76ca3dbe656fc
Jose Juan Reyes <neodevelop@gmail.com>
2021-08-10 12:58:20 -0500

    Add a test for Sequence

--- src/test/kotlin/com/makingdevs/FizzBuzzExpectTest.kt
+++ src/test/kotlin/com/makingdevs/FizzBuzzExpectTest.kt
@@ -1,20 +1,24 @@
 package com.makingdevs
 
 import io.kotest.core.spec.style.ExpectSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.*
 
 class FizzBuzzExpectTest : ExpectSpec() {
 
   init {
     context("FizzBuzz") {
-      expect("be 'Fizz' when a number is 3 mutiple") {
+      expect("'Fizz' when a number is 3 mutiple") {
         FizzBuzz().play(3) shouldBe "Fizz"
       }
-      expect("be 'Buzz' when a number is 5 mutiple") {
+      expect("'Buzz' when a number is 5 mutiple") {
         FizzBuzz().play(5) shouldBe "Buzz"
       }
-      expect("be 'FizzBuzz' when a number is 3 and 5 mutiple") {
+      expect("'FizzBuzz' when a number is 3 and 5 mutiple") {
         FizzBuzz().play(15) shouldBe "FizzBuzz"
+      }
+      expect("a FizzBuzz sequence....") {
+        val result = FizzBuzz().sequence(6)
+        result shouldBe arrayOf("1", "2", "Fizz", "4", "Buzz", "Fizz")
       }
     }
   }
